5a8072332f08c562c412f5397d7be723f07fc0d4
NguyenManhHung <nguyenmanhhung176472@gmail.com>
2023-11-16 13:56:16 +0700

    Bai Bao Cao

---
+++ views/frontend/footer.php
@@ -0,0 +1,62 @@
+<section class="hdl-footer pb-4">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-4 pt-4">
+            <h3 class="widgettilte">CHÚNG TÔI LÀ AI ?</h3>
+            <p class="pt-1">
+               Copyright@ 2024 DienloiShop là hệ thống bán sĩ và lẽ thời trang nam, nữ, trẻ em và quần áo thể thao,
+               mong muốn đem đến chất lượng tốt nhất cho khách hàng.
+            </p>
+            <p class="pt-1">
+               Địa chỉ: Biên Hoà - Đồng Nai
+            </p>
+            <p class="pt-1">
+               Điện thoại: 0961 804 507(call, zalo) - Email: nguyenmanhhung176472@gmail.com
+            </p>
+            <h3 class="widgettilte">MẠNG XÃ HỘI</h3>
+            <div class="social my-3">
+               <div class="facebook-icon">
+                  <a href="https://www.facebook.com/hungs.mabu">
+                     <i class="fab fa-facebook-f"></i>
+                  </a>
+               </div>
+               <div class="instagram-icon">
+                  <a href="#">
+                     <i class="fab fa-instagram"></i>
+                  </a>
+               </div>
+               <div class="google-icon">
+                  <a href="https://www.google.com.vn/?hl=vi">
+                     <i class="fab fa-google"></i>
+                  </a>
+               </div>
+               <div class="youtube-icon">
+                  <a href="https://www.youtube.com/channel/UCU5nPbtn_H5RgZiBXE1eFEQ">
+                     <i class="fab fa-youtube"></i>
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="col pt-4">
+         <iframe class="img-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31336.115423449504!2d106.7588129102111!3d10.962282414986404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174dbe0eab49ef7%3A0x4e153e94b230ffbf!2zQuG7rXUgTG9uZywgVGjDoG5oIHBo4buRIEJpw6puIEjDsmEsIMSQ4buTbmcgTmFpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1699946853131!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         </div>
+         <div class="col-md-4 pt-4">
+            <?php require_once "views/frontend/mod-footermenu.php" ?>
+         </div>
+         <div class="col-md-4 pt-4 text-end">
+            <h3 class="fs-5 text-end">
+               <strong>Lượt truy cập</strong>
+            </h3>
+            <p class="my-1">9888 lượt</p>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="dhl-copyright bg-dark py-3">
+   <div class="container text-center text-white">
+      Thiết kế bởi: Nguyễn Mạnh Hùng - Phone: 0961804507
+   </div>
+</section>
+</body>
+
+</html>
